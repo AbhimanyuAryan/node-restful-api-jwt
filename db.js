3b9d43a56dbd28b9138ca6ba39a0524914c357c2
Abhimanyu Aryan <abhimanyu.chat@gmail.com>
2018-09-25 12:47:08 +0530

    JWT Authentication

--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://yourMongoDBURIGoesHere');
+mongoose.connect('mongodb://testuser:test123@ds213183.mlab.com:13183/jwt', { useMongoClient: true });
